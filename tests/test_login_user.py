e676da6b2a62ca734f8ccf4b85a7f5168ca2e15b
MakTV19 <jakesonier@icloud.com>
2025-11-10 16:13:35 +0300

    Fixing test_login_user.py

--- tests/test_login_user.py
+++ tests/test_login_user.py
@@ -5,8 +5,6 @@ import allure
 from data.urls import Urls
 
 
-
-
 class TestLoginUser:
     @allure.title("Успешный логин существующего пользователя")
     def test_login_existing_user(self, create_user):
@@ -24,30 +22,15 @@ class TestLoginUser:
         assert response.json()["user"]["email"] == create_user["email"]
         assert response.json()["user"]["name"] == create_user["name"]
 
+    
     @allure.title("Логин с некорректными данными")
     @pytest.mark.parametrize("invalid_data", [
         {"email": "wrong@yandex.ru", "password": "password123"},
         {"email": f"test_{uuid.uuid4()}@yandex.ru", "password": "wrongpassword"}
     ])
+    
     def test_login_invalid_credentials(self, invalid_data):
         response = requests.post(Urls.LOGIN, json=invalid_data)
-        
-        assert response.status_code == 401
-        assert response.json() == {
-            "success": False,
-            "message": "email or password are incorrect"
-        }
-
-    @allure.title("Логин с пустыми данными")
-    @pytest.mark.parametrize("empty_field", ["email", "password"])
-    def test_login_empty_fields(self, empty_field):
-        login_data = {
-            "email": "test@yandex.ru",
-            "password": "password123"
-        }
-        login_data[empty_field] = ""
-        
-        response = requests.post(Urls.LOGIN, json=login_data)
         
         assert response.status_code == 401
         assert response.json() == {
